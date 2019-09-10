dec87638e34309e47ecd9115b975e521421b313a
Montana Bowman <mbowman@mbowman-dev.striketechnologies.com>
2019-09-10 16:13:13 -0400

    Updated the diff to be a seperate field

--- js/scores.js
+++ js/scores.js
@@ -1,20 +1,20 @@
 tableCss="class=\"w3-table-all\" style=\"width: 500px;\""
-tableHeader = "<table " + tableCss + "><tr><th>Team</th><th>Wins</th><th>Losses</th><th>Diff A</th><th>Diff B</th><th>Diff C</th></tr>";
+tableHeader = "<table " + tableCss + "><tr><th>Team</th><th>Wins</th><th>Losses</th><th>Diff</th><th>A</th><th>B</th><th>C</th></tr>";
 endTable = "</table>";
 scores = {};
 
 blankScores=
 {
-    "A":{"win":0,"loss":0,"A":0,"B":0,"C":0},
-    "B":{"win":0,"loss":0,"A":0,"B":0,"C":0},
-    "C":{"win":0,"loss":0,"A":0,"B":0,"C":0}
+    "A":{"win":0,"loss":0,"diff":0,"A":0,"B":0,"C":0},
+    "B":{"win":0,"loss":0,"diff":0,"A":0,"B":0,"C":0},
+    "C":{"win":0,"loss":0,"diff":0,"A":0,"B":0,"C":0}
 }
 
 function displayScores() {
     table = tableHeader;
-    table += "<tr><th>A</th><th>" + scores["A"]["win"] + "</th><th>" + scores["A"]["loss"] + "</th><th>" + scores["A"]["A"]  + "</th><th>" + scores["A"]["B"]  + "</th><th>" + scores["A"]["C"] + "</th>" 
-    table += "<tr><th>B</th><th>" + scores["B"]["win"] + "</th><th>" + scores["B"]["loss"] + "</th><th>" + scores["B"]["A"]  + "</th><th>" + scores["B"]["B"]  + "</th><th>" + scores["B"]["C"] + "</th>" 
-    table += "<tr><th>C</th><th>" + scores["C"]["win"] + "</th><th>" + scores["C"]["loss"] + "</th><th>" + scores["C"]["A"]  + "</th><th>" + scores["C"]["B"]  + "</th><th>" + scores["C"]["C"] + "</th>" 
+    table += "<tr><th>A</th><th>" + scores["A"]["win"] + "</th><th>" + scores["A"]["loss"] + "</th><th>" + scores["A"]["diff"] + "</th><th>" + scores["A"]["A"]  + "</th><th>" + scores["A"]["B"]  + "</th><th>" + scores["A"]["C"] + "</th>";
+    table += "<tr><th>B</th><th>" + scores["B"]["win"] + "</th><th>" + scores["B"]["loss"] + "</th><th>" + scores["B"]["diff"] + "</th><<th>" + scores["B"]["A"]  + "</th><th>" + scores["B"]["B"]  + "</th><th>" + scores["B"]["C"] + "</th>";
+    table += "<tr><th>C</th><th>" + scores["C"]["win"] + "</th><th>" + scores["C"]["loss"] + "</th><th>" + scores["C"]["diff"] + "</th><<th>" + scores["C"]["A"]  + "</th><th>" + scores["C"]["B"]  + "</th><th>" + scores["C"]["C"] + "</th>";
     table += endTable
     document.getElementById("scores").innerHTML = table;
 }
@@ -41,10 +41,16 @@ function recordGame() {
         //can't play yourself
 	return;
     }
+
+    //Adjust scores of the winner
     scores[w]["win"]++;
-    scores[w][l]+=diff;
+    scores[w][l]++;
+    scores[w]["diff"]+=diff;
+
+    //Adjust scores of the loser
     scores[l]["loss"]++;
-    scores[l][w]-=diff;
+    scores[l][w]--;
+    scores[l]["diff"]-=diff;
     sendScores();
 }
 
